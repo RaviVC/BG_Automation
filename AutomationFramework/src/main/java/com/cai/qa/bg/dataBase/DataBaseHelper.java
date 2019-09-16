f3abaa462981e2196fda1a838ae09a4fb4ff9297
Ravi Chandrika <Ravi.Chandrika@cai.io>
2019-09-16 21:22:50 +0530

    AutomationFramework

---
+++ AutomationFramework/src/main/java/com/cai/qa/bg/dataBase/DataBaseHelper.java
@@ -0,0 +1,5 @@
+package com.cai.qa.bg.dataBase;
+
+public class DataBaseHelper {
+
+}
---
+++ AutomationFramework/src/test/java/com/cai/qa/bg/testScripts/BGG/CreateBGGetawayPackage.java
@@ -0,0 +1,25 @@
+package com.cai.qa.bg.testScripts.BGG;
+
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.cai.qa.bg.javaScript.JavaScriptHelper;
+import com.cai.qa.bg.log4j.LoggerHelp;
+import com.cai.qa.bg.testBase.BaseTest;
+import com.cai.qa.bg.testBase.TestBase;
+
+public class CreateBGGetawayPackage extends BaseTest { 
+	
+	private Logger log = LoggerHelp.getLogger(CreateBGGetawayPackage.class);
+	
+	
+	@Test
+	public void Launch(){
+		test = extent.createTest("Launch BGG");
+		test.log(Status.INFO,"Test Case Execution Started");
+		launchApp(CONFIG.getProperty("BGG_URL"), "stg");
+	}
+}
